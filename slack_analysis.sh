5540ca4b46f790409d0e98ebb8fc0e8e18eae6c2
juanjse13 <restrepotorojuanjose@gmail.com>
2023-12-29 16:56:45 +0100

    ADD: Add changes to script to analyze slack of the components

--- slack_analysis.sh
+++ slack_analysis.sh
@@ -2,4 +2,18 @@ if [ "$#" -ne 1 ]; then
     echo "Usage: $0 threshold"
     exit 1
 fi
-tclsh slack_analysis.tcl $1
+
+
+
+echo "Number of netlists with slacks lower than the threshold when Th = $1" >> Blocks_slack.txt
+echo " " >> Blocks_slack.txt
+tclsh slack_analysis.tcl $1 >> Blocks_slack.txt
+
+tclsh slack_block.tcl $1 load_store_unit_i >> Blocks_slack.txt
+tclsh slack_block.tcl $1 id_stage_i >> Blocks_slack.txt
+tclsh slack_block.tcl $1 sleep_unit_i >> Blocks_slack.txt
+tclsh slack_block.tcl $1 if_stage_i >> Blocks_slack.txt
+tclsh slack_block.tcl $1 cs_registers_i >> Blocks_slack.txt
+tclsh slack_block.tcl $1 ex_stage_i >> Blocks_slack.txt
+
+echo "-------------------------------------------------------------------------------------------------------" >> Blocks_slack.txt
