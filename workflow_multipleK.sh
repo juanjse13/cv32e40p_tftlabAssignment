fdc9183a6195f48ead6d22fed9db518310869334
juanjse13 <restrepotorojuanjose@gmail.com>
2024-01-06 15:06:14 +0100

    FIX: fix script to work with floating point

--- workflow_multipleK.sh
+++ workflow_multipleK.sh
@@ -1,10 +1,3 @@
-read -p "Select the first k: " initialK
-echo "First K: $initialK"
-read -p "Select the step: " step
-echo "First K: $step"
-read -p "Select the last k " lastK
-echo "Last L: $lastK"
-
 #make clean
 make pt/generate_gsf #generate gsf file
 make compile_sbst #cross compilation
@@ -14,15 +7,9 @@ make zoix/compile #To compile the zoix
 make zoix/lsim #To verify th eVCD stimuli
 
 
-
-startk=$initialK
-endk=$lastK
-stepk=$stepK
-
-# Using seq to generate a sequence of floating-point values
-for currentk in $(seq $startk $stepk $endk); do
-    # Your code here, using $currentk as the non-integer value
-    echo "Current value of K: $currentk"
-    make zoix/fgen/sdd K=$currentk
-    make zoix/fsim FAULT_LIST=run/zoix_timing/cv32e40p_top_sdd_K$currentk.rpt 2>&1 | tail -n 19 > fault_coverage_rpt$currentk.txt
+seq $1 $2 $3 | \
+while read currentk
+do
+    make zoix/fgen/sdd K=$currentk #Generate fault list
+    make zoix/fsim FAULT_LIST=run/zoix_timing/cv32e40p_top_sdd_K$1.rpt 2>&1 | tail -n 19 > fault_coverage_rpt.txt  #Inject faults and get coverage
 done
