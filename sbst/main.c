9d1840096c4626dbfbbeebd5e348a00ce004e153
juanjse13 <restrepotorojuanjose@gmail.com>
2024-01-09 17:00:58 +0100

    FIX: Fix files to simulate

--- sbst/main.c
+++ sbst/main.c
@@ -3,6 +3,10 @@
 
 const signature_t const GOLDEN_SIGNATURES[NUMTESTS] = {
 	0xCAFECAFE,	// TEST1
+	0xCAFECAFE,	// TEST2
+	0xCAFECAFE,	// TEST3
+	0xCAFECAFE,	// TEST4
+
 };
 
 int main()
@@ -12,6 +16,9 @@ int main()
 	
 	signatures[TEST1] = test1();
 	signatures[TEST2] = test2();
+	signatures[TEST3] = atpg_patterns_DIV();
+	signatures[TEST4] = div_test();	
+	
 
 	for(i=0; i<NUMTESTS; i++) {
 		fails += signatures[i] != GOLDEN_SIGNATURES[i];
--- sbst/tests.h
+++ sbst/tests.h
@@ -3,12 +3,17 @@
 
 #define TEST1 0
 #define TEST2 1
-#define NUMTESTS 2
+#define TEST3 2
+#define TEST4 3
+#define NUMTESTS 4
 
 typedef unsigned int signature_t;
 
 extern signature_t test1(void);
 extern signature_t test2(void);
+extern signature_t atpg_patterns_DIV(void);
+extern signature_t div_test(void);
+
 
 
 #endif /*TESTS_H */
