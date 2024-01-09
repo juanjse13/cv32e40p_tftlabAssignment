4190f750c78052b3d21041b2e9fdee9545f2b863
juanjse13 <restrepotorojuanjose@gmail.com>
2024-01-09 17:02:06 +0100

    FIX: update multiple k script

--- workflow_multipleK.sh
+++ workflow_multipleK.sh
@@ -11,5 +11,12 @@ seq $1 $2 $3 | \
 while read currentk
 do
     make zoix/fgen/sdd K=$currentk #Generate fault list
-    make zoix/fsim FAULT_LIST=run/zoix_timing/cv32e40p_top_sdd_K$currentk.rpt 2>&1 | tail -n 19 > fault_coverage_rpt.txt  #Inject faults and get coverage
+    old_filename="run/zoix_timing/cv32e40p_top_sdd_K$currentk.rpt"
+    new_filename="run/zoix_timing/cv32e40p_top_sdd_K.rpt"
+    mv "$old_filename" "$new_filename"
+    make zoix/fsim FAULT_LIST=run/zoix_timing/cv32e40p_top_sdd_K.rpt 2>&1 | tail -n 19 > fault_coverage_rpt.txt  #Inject faults and get coverage
+    echo "$currentk" >> K_values.txt
+
+    tclsh extract.tcl
+
 done
