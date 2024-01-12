8adc697e1133ee2baf417ac1879276318a3a249a
juanjse13 <restrepotorojuanjose@gmail.com>
2024-01-12 09:54:00 +0100

    ADD: add more results for comparing

--- sbst/main.c
+++ sbst/main.c
@@ -13,8 +13,8 @@ int main()
 	signature_t signatures[NUMTESTS];
 	
 	signatures[TEST1] = test1();
-	//signatures[TEST2] = atpg_patterns_DIV();
-	signatures[TEST3] = div_test();	
+	signatures[TEST2] = atpg_patterns_DIV();
+	//signatures[TEST3] = div_test();	
 	
 
 	for(i=0; i<NUMTESTS; i++) {
--- sbst/tests.h
+++ sbst/tests.h
@@ -2,15 +2,15 @@
 #define TESTS_H
 
 #define TEST1 0
-//#define TEST2 1
-#define TEST3 2
+#define TEST2 1
+//#define TEST3 2
 #define NUMTESTS 2
 
 typedef unsigned int signature_t;
 
 extern signature_t test1(void);
-//extern signature_t atpg_patterns_DIV(void);
-extern signature_t div_test(void);
+extern signature_t atpg_patterns_DIV(void);
+//extern signature_t div_test(void);
 
 
 
